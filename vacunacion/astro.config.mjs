173306de88df803d89efd4b8addb80387a7e0cff
Sofmer7 <promyios27@gmail.com>
2025-10-03 10:19:41 -0600

    Github Actions

--- vacunacion/astro.config.mjs
+++ vacunacion/astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: "https://Sofmer7.github.io",
+  base: "/Vacunacion",
 });
